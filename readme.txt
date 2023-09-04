113783ddfeccd9896563b9b7c07b0d9a324d5afc
Matias Ignacio Paredes Marquez <mat.ignaciom95@gmail.com>
2023-09-04 12:30:08 -0300

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,27 @@
+# Documentación del Proyecto: Carrito de Compras
+
+## Descripción
+
+El proyecto "Carrito de Compras" es una aplicación web que permite a los usuarios agregar, eliminar y comprar productos. Los productos seleccionados se almacenan en el carrito de compras y pueden ser guardados en el almacenamiento local del navegador.
+
+## Instrucciones
+
+1. **Agregar Productos:** Haz clic en "Agregar" junto a un producto para agregarlo al carrito.
+
+2. **Ver el Carrito:** Los productos agregados aparecerán en la sección "Productos en el Carrito". Puedes eliminar productos y ajustar sus cantidades.
+
+3. **Eliminar Productos:** Haz clic en "Eliminar" en el carrito, ingresa la cantidad y ajusta el carrito.
+
+4. **Comprar Productos:** Haz clic en "Comprar" para guardar los productos en el almacenamiento local y finalizar la compra.
+
+## Funciones Principales
+
+- `actualizarCarrito()`: Actualiza la visualización del carrito y muestra productos, cantidades y precio total.
+
+- `guardarCarritoEnLocalStorage()`: Guarda el carrito en el almacenamiento local después de modificaciones.
+
+## Tecnologías
+
+- HTML5
+- CSS3 (Bootstrap)
+- JavaScript (Vanilla)
---
+++ script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    const productList = document.getElementById('product-list');
+    const buyButton = document.getElementById('buy-btn');
+  
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    actualizarCarrito();
+  
+    productList.addEventListener('click', (evento) => {
+      if (evento.target.classList.contains('add-to-cart')) {
+        const producto = evento.target.parentElement;
+        const idProducto = producto.dataset.id;
+        const precioProducto = parseFloat(producto.dataset.price);
+  
+        const itemExistente = cart.find(item => item.id === idProducto);
+        if (itemExistente) {
+          itemExistente.cantidad += 1;
+        } else {
+          cart.push({ id: idProducto, precio: precioProducto, cantidad: 1 });
+        }
+  
+        actualizarCarrito();
+      }
+    });
+  
+    cartItems.addEventListener('click', (evento) => {
+      if (evento.target.classList.contains('remove')) {
+        const idItem = evento.target.parentElement.dataset.id;
+        const cantidadEliminar = parseInt(prompt('Ingrese la cantidad a eliminar:', '1'));
+  
+        if (!isNaN(cantidadEliminar) && cantidadEliminar > 0) {
+          const item = cart.find(item => item.id === idItem);
+          if (item) {
+            if (cantidadEliminar >= item.cantidad) {
+              cart = cart.filter(item => item.id !== idItem);
+            } else {
+              item.cantidad -= cantidadEliminar;
+            }
+  
+            actualizarCarrito();
+          }
+        }
+      }
+    });
+  
+    buyButton.addEventListener('click', () => {
+      localStorage.setItem('cart', JSON.stringify(cart));
+      alert('Compra realizada. Los productos se han guardado en el carrito.');
+      cart = [];
+      actualizarCarrito();
+    });
+  
+    function actualizarCarrito() {
+      cartItems.innerHTML = '';
+      let total = 0;
+  
+      cart.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('cart-item');
+        li.dataset.id = item.id;
+        li.innerHTML = `${item.cantidad}x Producto ${item.id} - $${item.precio * item.cantidad} <button class="remove btn btn-danger">Eliminar</button>`;
+        cartItems.appendChild(li);
+        total += item.precio * item.cantidad;
+      });
+  
+      cartTotal.textContent = total.toFixed(2);
+    }
+  });
+  
